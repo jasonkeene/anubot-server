86e3d4ce32d785fe40dbda9891f92b57c06c085a
Jason Keene <jasonkeene@gmail.com>
2017-03-10 07:15:58 -0700

    Fix migration uniq constraints

--- store/migrations/1489118510_remove_null_fields.up.sql
+++ store/migrations/1489118510_remove_null_fields.up.sql
@@ -11,10 +11,12 @@ ALTER TABLE "user" ALTER COLUMN username SET NOT NULL;
 DELETE FROM "user" WHERE password_hash IS NULL;
 ALTER TABLE "user" ALTER COLUMN password_hash SET NOT NULL;
 
+ALTER TABLE "user" DROP CONSTRAINT user_streamer_id_key;
 UPDATE "user" set streamer_id = 0 WHERE streamer_id IS NULL;
 ALTER TABLE "user" ALTER COLUMN streamer_id SET NOT NULL;
 ALTER TABLE "user" ALTER COLUMN streamer_id SET DEFAULT 0;
 
+ALTER TABLE "user" DROP CONSTRAINT user_streamer_username_key;
 UPDATE "user" set streamer_username = '' WHERE streamer_username IS NULL;
 ALTER TABLE "user" ALTER COLUMN streamer_username SET NOT NULL;
 ALTER TABLE "user" ALTER COLUMN streamer_username SET DEFAULT '';
@@ -23,10 +25,12 @@ UPDATE "user" set streamer_oauth_data = '' WHERE streamer_oauth_data IS NULL;
 ALTER TABLE "user" ALTER COLUMN streamer_oauth_data SET NOT NULL;
 ALTER TABLE "user" ALTER COLUMN streamer_oauth_data SET DEFAULT '';
 
+ALTER TABLE "user" DROP CONSTRAINT user_bot_id_key;
 UPDATE "user" set bot_id = 0 WHERE bot_id IS NULL;
 ALTER TABLE "user" ALTER COLUMN bot_id SET NOT NULL;
 ALTER TABLE "user" ALTER COLUMN bot_id SET DEFAULT 0;
 
+ALTER TABLE "user" DROP CONSTRAINT user_bot_username_key;
 UPDATE "user" set bot_username = '' WHERE bot_username IS NULL;
 ALTER TABLE "user" ALTER COLUMN bot_username SET NOT NULL;
 ALTER TABLE "user" ALTER COLUMN bot_username SET DEFAULT '';
@@ -42,7 +46,7 @@ ALTER TABLE nonce ALTER COLUMN created SET NOT NULL;
 DELETE FROM nonce WHERE modified IS NULL;
 ALTER TABLE nonce ALTER COLUMN modified SET NOT NULL;
 
-UPDATE nonce set nonce = '' WHERE nonce IS NULL;
+DELETE FROM nonce WHERE nonce IS NULL;
 ALTER TABLE nonce ALTER COLUMN nonce SET NOT NULL;
 
 -- message table
